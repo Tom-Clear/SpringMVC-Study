6a7c0fd58b6223a030ba9625685561b7d93f7c24
cxp <Chenxiping@xiyundata.com>
2020-06-30 22:31:40 +0800

    day01

---
+++ springmvc-mybatis/src/main/java/com/wsw/app/dao/NewsManager.java
@@ -0,0 +1,44 @@
+package com.wsw.app.dao;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.util.HashMap;
+
+public class NewsManager {
+
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static void main(String[] args) throws Exception{
+        var resource = "mybatis-config.xml";
+        // 使用 Resources 工具从类路径下加载指定文件
+        var inputStream = Resources.getResourceAsStream(resource);
+        // 构建 SqlSessionFactory
+        sqlSessionFactory = new SqlSessionFactoryBuilder()
+                .build(inputStream);
+        // 打开 Session
+        var sqlSession = sqlSessionFactory.openSession();
+        insertTest(sqlSession);
+    }
+    public static void  insertTest(SqlSession sqlSession){
+        // 创建消息实例
+        var news = new HashMap<String,String>();
+        // 设置消息标题和消息内容
+        news.put("title","王世文的女朋友");
+        news.put("content","她是郑爽");
+        try {
+            // 调用 insert 方法执行 SQL 语句
+            var n = sqlSession.insert("com.wsw.app.dao.NewsMapper.saveNews",news);
+            System.out.printf("插入了%d条数据%n",n);
+        }catch (Exception ex){
+
+        }finally {
+            // 提交事务
+            sqlSession.commit();
+            // 关闭资源
+            sqlSession.close();
+        }
+    }
+}
